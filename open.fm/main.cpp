39061342b97e9fe4afbfa43f92e87c9d56662a15
Kamil Klimek <naresh@tlen.pl>
2013-02-26 22:08:24 +0100

    Updated to QRpi namespace

--- open.fm/main.cpp
+++ open.fm/main.cpp
@@ -6,15 +6,15 @@
 
 #include <QRpiGpio>
 
-#define LCD_RS QRpiGpio::Pin_CE1
-#define LCD_E  QRpiGpio::Pin_CE0
-#define LCD_D4 QRpiGpio::Pin_GPIO6
-#define LCD_D5 QRpiGpio::Pin_GPIO5
-#define LCD_D6 QRpiGpio::Pin_GPIO4
-#define LCD_D7 QRpiGpio::Pin_GPIO1
+#define LCD_RS QRpi::P1Header::Pin_CE1
+#define LCD_E  QRpi::P1Header::Pin_CE0
+#define LCD_D4 QRpi::P1Header::Pin_GPIO6
+#define LCD_D5 QRpi::P1Header::Pin_GPIO5
+#define LCD_D6 QRpi::P1Header::Pin_GPIO4
+#define LCD_D7 QRpi::P1Header::Pin_GPIO1
 
-#define LCD_MODE_CMD QRpiGpio::PinValue_High
-#define LCD_MODE_CHR QRpiGpio::PinValue_Low
+#define LCD_MODE_CMD QRpi::P1Header::PinValue_High
+#define LCD_MODE_CHR QRpi::P1Header::PinValue_Low
 
 #define LCD_LINE_1 0x80
 #define LCD_LINE_2 0xC0
@@ -24,64 +24,64 @@ class DebugOutput: public QObject
 {
 	Q_OBJECT
 public slots:
-	void interrupted(QRpiGpio::Pin pin, QRpiGpio::PinValue val) {
+	void interrupted(QRpi::P1Header::Pin pin, QRpi::P1Header::PinValue val) {
 		qDebug() << Q_FUNC_INFO << pin << val;
 	}
 
-	void fallen(QRpiGpio::Pin pin) {
+	void fallen(QRpi::P1Header::Pin pin) {
 		qDebug() << Q_FUNC_INFO << pin;
 	}
 
-	void raised(QRpiGpio::Pin pin) {
+	void raised(QRpi::P1Header::Pin pin) {
 		qDebug() << Q_FUNC_INFO << pin;
 	}
 };
 
-void lcd_pulse(QRpiGpio * gpio)
+void lcd_pulse(QRpi::P1Header * gpio)
 {
 	gpio->delayUs(50);
 }
 
-void lcd_delay(QRpiGpio * gpio)
+void lcd_delay(QRpi::P1Header * gpio)
 {
 	gpio->delayUs(50);
 }
 
-void lcd_reset_data(QRpiGpio * gpio)
+void lcd_reset_data(QRpi::P1Header * gpio)
 {
-	gpio->writePin(LCD_D4, QRpiGpio::PinValue_High);
-	gpio->writePin(LCD_D5, QRpiGpio::PinValue_High);
-	gpio->writePin(LCD_D6, QRpiGpio::PinValue_High);
-	gpio->writePin(LCD_D7, QRpiGpio::PinValue_High);
+	gpio->writePin(LCD_D4, QRpi::P1Header::PinValue_High);
+	gpio->writePin(LCD_D5, QRpi::P1Header::PinValue_High);
+	gpio->writePin(LCD_D6, QRpi::P1Header::PinValue_High);
+	gpio->writePin(LCD_D7, QRpi::P1Header::PinValue_High);
 }
 
-void lcd_toggle_enable(QRpiGpio * gpio)
+void lcd_toggle_enable(QRpi::P1Header * gpio)
 {
 	lcd_delay(gpio);
-	gpio->writePin(LCD_E, QRpiGpio::PinValue_Low);
+	gpio->writePin(LCD_E, QRpi::P1Header::PinValue_Low);
 	lcd_pulse(gpio);
-	gpio->writePin(LCD_E, QRpiGpio::PinValue_High);
+	gpio->writePin(LCD_E, QRpi::P1Header::PinValue_High);
 	lcd_delay(gpio);
 }
 
-void lcd_byte(QRpiGpio * gpio, int bits,  QRpiGpio::PinValue mode)
+void lcd_byte(QRpi::P1Header * gpio, int bits,  QRpi::P1Header::PinValue mode)
 {
 	gpio->writePin(LCD_RS, mode);
 
 	lcd_reset_data(gpio);
 
 	if ((bits & 0x10) == 0x10)
-		gpio->writePin(LCD_D4, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D4, QRpi::P1Header::PinValue_Low);
 
 
 	if ((bits & 0x20) == 0x20)
-		gpio->writePin(LCD_D5, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D5, QRpi::P1Header::PinValue_Low);
 
 	if ((bits & 0x40) == 0x40)
-		gpio->writePin(LCD_D6, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D6, QRpi::P1Header::PinValue_Low);
 
 	if ((bits & 0x80) == 0x80)
-		gpio->writePin(LCD_D7, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D7, QRpi::P1Header::PinValue_Low);
 
 
 	lcd_toggle_enable(gpio);
@@ -89,21 +89,21 @@ void lcd_byte(QRpiGpio * gpio, int bits,  QRpiGpio::PinValue mode)
 	lcd_reset_data(gpio);
 
 	if ((bits & 0x01) == 0x01)
-		gpio->writePin(LCD_D4, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D4, QRpi::P1Header::PinValue_Low);
 
 	if ((bits & 0x02) == 0x02)
-		gpio->writePin(LCD_D5, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D5, QRpi::P1Header::PinValue_Low);
 
 	if ((bits & 0x04) == 0x04)
-		gpio->writePin(LCD_D6, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D6, QRpi::P1Header::PinValue_Low);
 
 	if ((bits & 0x08) == 0x08)
-		gpio->writePin(LCD_D7, QRpiGpio::PinValue_Low);
+		gpio->writePin(LCD_D7, QRpi::P1Header::PinValue_Low);
 
 	lcd_toggle_enable(gpio);
 }
 
-void lcd_init(QRpiGpio * gpio)
+void lcd_init(QRpi::P1Header * gpio)
 {
 	lcd_byte(gpio, 0x33, LCD_MODE_CMD);
 	lcd_byte(gpio, 0x32, LCD_MODE_CMD);
@@ -113,7 +113,7 @@ void lcd_init(QRpiGpio * gpio)
 	lcd_byte(gpio, 0x01, LCD_MODE_CMD);
 }
 
-void lcd_write_string(QRpiGpio *gpio, QString str, bool secondLine = false)
+void lcd_write_string(QRpi::P1Header *gpio, QString str, bool secondLine = false)
 {
 	lcd_byte(gpio, secondLine ? LCD_LINE_2 : LCD_LINE_1, LCD_MODE_CMD);
 
@@ -130,18 +130,18 @@ int main(int argc, char * argv[])
 	QCoreApplication app(argc, argv);
 
 	DebugOutput out;
-	QRpiGpio gpio;
+	QRpi::P1Header gpio;
 
-	QObject::connect(&gpio, SIGNAL(interrupted(QRpiGpio::Pin, QRpiGpio::PinValue)), &out, SLOT(interrupted(QRpiGpio::Pin, QRpiGpio::PinValue)));
-	QObject::connect(&gpio, SIGNAL(fallen(QRpiGpio::Pin)), &out, SLOT(fallen(QRpiGpio::Pin)));
-	QObject::connect(&gpio, SIGNAL(raised(QRpiGpio::Pin)), &out, SLOT(raised(QRpiGpio::Pin)));
+	QObject::connect(&gpio, SIGNAL(interrupted(QRpi::P1Header::Pin, QRpi::P1Header::PinValue)), &out, SLOT(interrupted(QRpi::P1Header::Pin, QRpi::P1Header::PinValue)));
+	QObject::connect(&gpio, SIGNAL(fallen(QRpi::P1Header::Pin)), &out, SLOT(fallen(QRpi::P1Header::Pin)));
+	QObject::connect(&gpio, SIGNAL(raised(QRpi::P1Header::Pin)), &out, SLOT(raised(QRpi::P1Header::Pin)));
 
-	gpio.setOutput(LCD_E, QRpiGpio::PinValue_High);
-	gpio.setOutput(LCD_RS, QRpiGpio::PinValue_High);
-	gpio.setOutput(LCD_D4, QRpiGpio::PinValue_High);
-	gpio.setOutput(LCD_D5, QRpiGpio::PinValue_High);
-	gpio.setOutput(LCD_D6, QRpiGpio::PinValue_High);
-	gpio.setOutput(LCD_D7, QRpiGpio::PinValue_High);
+	gpio.setOutput(LCD_E, QRpi::P1Header::PinValue_High);
+	gpio.setOutput(LCD_RS, QRpi::P1Header::PinValue_High);
+	gpio.setOutput(LCD_D4, QRpi::P1Header::PinValue_High);
+	gpio.setOutput(LCD_D5, QRpi::P1Header::PinValue_High);
+	gpio.setOutput(LCD_D6, QRpi::P1Header::PinValue_High);
+	gpio.setOutput(LCD_D7, QRpi::P1Header::PinValue_High);
 
 	lcd_init(&gpio);
 
@@ -149,7 +149,7 @@ int main(int argc, char * argv[])
 	lcd_write_string(&gpio, "Testing", true);
 
 
-	QRpiGpio::Pin pinNo = QRpiGpio::Pin_GPIO7;
+	QRpi::P1Header::Pin pinNo = QRpi::P1Header::Pin_GPIO7;
 
 	gpio.setInput(pinNo);
 
